89b8bbaf2e1110ab9462a8c3d02fdc3c0b6d6aab
Xeraph <jeongdo.dev@gmail.com>
2023-11-25 00:16:43 +0900

    chore: theme

---
+++ src/style/theme.ts
@@ -0,0 +1,19 @@
+export const THEME = {
+  BACKGROUND_COLOR: {
+    BUTTON: "#007fff",
+  },
+  BORDER_RADIUS: {
+    L: "10px",
+    M: "7px",
+    S: "5px",
+  },
+  BUTTON_SIZE: {
+    L: {
+      WIDTH: "155px",
+      HEIGHT: "55px",
+    },
+  },
+  COLOR: {
+    WHITE: "#FFFFFF",
+  },
+};
