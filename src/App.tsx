4c0359105264db7b3e50e02d93e863daaa19ecfb
Xeraph <jeongdo.dev@gmail.com>
2023-11-24 23:27:04 +0900

    chore: firebase 연동

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.tsx
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const getFirebaseData = async () => {
+    const querySnapshot = await getDocs(collection(db, "test"));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
+      console.log(doc.data());
+    });
+  };
+  getFirebaseData();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  return <div>hello world!!</div>;
+};
 
-export default App
+export default App;
