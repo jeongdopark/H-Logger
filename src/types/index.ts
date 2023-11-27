0f8d9cb540d3074813475232cfbda7ec69c68c9c
Xeraph <jeongdo.dev@gmail.com>
2023-11-28 05:07:35 +0900

    feat: calendar data type

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export interface ICalendar {
+  dailyLog: IDailyLog;
+  exercise: IExercise[];
+  meal: IMeal[];
+}
+
+export interface IDailyLog {
+  log: string;
+  score: number;
+}
+
+export interface IExercise {
+  category: string;
+  time: "15분" | "30분" | "45분" | "1시간" | "1시간 15분" | "1시간 30분" | "1시간 45분" | "2시간";
+}
+
+export interface IMeal {
+  menu: string[];
+  time: "아침" | "아점" | "점심" | "점저" | "저녁" | "야식";
+}
