2567b929850686f3493a230445641b74934fbdb3
Xeraph <jeongdo.dev@gmail.com>
2023-11-29 05:57:06 +0900

    feat: nav router

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,24 +2,25 @@ import { S } from "./styled";
 import useMovePage from "../../hooks/useMovePage";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthProvider";
+import { PATH_NUMBER } from "../../const/path";
 
 const Header = () => {
   const { isLogin, setIsLogin } = useContext(AuthContext);
-
   const [routerHandler] = useMovePage();
 
   const handleSignout = () => {
     setIsLogin(false);
     localStorage.removeItem("uid");
+    routerHandler({ num: PATH_NUMBER.LANDING });
   };
 
   return (
     <S.HeaderContainer>
-      <S.NavElement onClick={() => routerHandler({ num: 1 })}>HLogger</S.NavElement>
+      <S.NavElement onClick={() => routerHandler({ num: PATH_NUMBER.LANDING })}>HLogger</S.NavElement>
       {isLogin && (
         <S.NavWrapper>
-          <div onClick={() => routerHandler({ num: 2 })}>Calendar</div>
-          <div onClick={() => routerHandler({ num: 3 })}>Record</div>
+          <div onClick={() => routerHandler({ num: PATH_NUMBER.CALENDAR })}>Calendar</div>
+          <div onClick={() => routerHandler({ num: PATH_NUMBER.RECORD })}>Record</div>
           <div onClick={handleSignout}>Signout</div>
         </S.NavWrapper>
       )}
