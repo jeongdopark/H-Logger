693c5b88195436637366815df7066a6d1b4ac743
Xeraph <jeongdo.dev@gmail.com>
2023-11-29 04:33:37 +0900

    feat: login header 분기처리

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,16 +1,28 @@
 import { S } from "./styled";
 import useMovePage from "../../hooks/useMovePage";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthProvider";
 
 const Header = () => {
+  const { isLogin, setIsLogin } = useContext(AuthContext);
+
   const [routerHandler] = useMovePage();
+
+  const handleSignout = () => {
+    setIsLogin(false);
+    localStorage.removeItem("uid");
+  };
 
   return (
     <S.HeaderContainer>
       <S.NavElement onClick={() => routerHandler({ num: 1 })}>HLogger</S.NavElement>
-      <S.NavWrapper>
-        <div onClick={() => routerHandler({ num: 2 })}>Calendar</div>
-        <div onClick={() => routerHandler({ num: 3 })}>Record</div>
-      </S.NavWrapper>
+      {isLogin && (
+        <S.NavWrapper>
+          <div onClick={() => routerHandler({ num: 2 })}>Calendar</div>
+          <div onClick={() => routerHandler({ num: 3 })}>Record</div>
+          <div onClick={handleSignout}>Signout</div>
+        </S.NavWrapper>
+      )}
     </S.HeaderContainer>
   );
 };
