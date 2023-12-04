a603c91b983ade2cb3edb963a16016f8f8abc18d
Xeraph <jeongdo.dev@gmail.com>
2023-12-05 04:08:56 +0900

    refactor: BrowserRouter  -> createBrowserRouter:

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import Router from "./routes/Router";
+import { RouterInfo } from "./routes/Router";
 import { GlobalStyle } from "./style/init";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./context/AuthProvider";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 const queryClient = new QueryClient();
+
+const RouterObject = createBrowserRouter(RouterInfo);
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <GlobalStyle />
       <AuthProvider>
-        <Router />
+        <RouterProvider router={RouterObject} />
       </AuthProvider>
     </QueryClientProvider>
   );
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,4 +1,3 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PATH_NAME } from "../const/path";
 import Calendar from "../pages/calendar/Calendar";
 import Landing from "../pages/landing/Landing";
@@ -7,20 +6,36 @@ import Layout from "../components/layout/Layout";
 import Record from "../pages/record/Record";
 import Mission from "../pages/mission/Mission";
 
-const Router = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path={PATH_NAME.CALENDAR_DETAIL} element={<CalendarDetail />} />
-          <Route path={PATH_NAME.CALENDAR} element={<Calendar />} />
-          <Route path={PATH_NAME.RECORD} element={<Record />} />
-          <Route path={PATH_NAME.MISSION} element={<Mission />} />
-          <Route path={PATH_NAME.LANDING} element={<Landing />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-};
-
-export default Router;
+export const RouterInfo = [
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+        label: "main",
+      },
+      {
+        path: PATH_NAME.CALENDAR,
+        element: <Calendar />,
+        label: "calendar",
+      },
+      {
+        path: PATH_NAME.CALENDAR_DETAIL,
+        element: <CalendarDetail />,
+        label: "calendarDetail",
+      },
+      {
+        path: PATH_NAME.MISSION,
+        element: <Mission />,
+        label: "mission",
+      },
+      {
+        path: PATH_NAME.RECORD,
+        element: <Record />,
+        label: "record",
+      },
+    ],
+  },
+];
