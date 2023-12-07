3f8d3fb5263e4e05b847f76b0615c5f567613740
Xeraph <jeongdo.dev@gmail.com>
2023-12-08 00:42:42 +0900

    feat: splitVendorChunkPlugin, manualChunks 빌드 최적화

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import { splitVendorChunkPlugin } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), splitVendorChunkPlugin()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes("firebase")) {
+            return "@firebase";
+          }
+          if (id.includes("date-fns")) {
+            return "@date-fns";
+          }
+          if (id.includes("react-router-dom") || id.includes("react-router")) {
+            return "@react-router";
+          }
+          if (id.includes("react-datepicker")) {
+            return "@react-datepicker";
+          }
+          if (id.includes("tanstack")) {
+            return "@react-query";
+          }
+        },
+      },
+    },
+  },
+});
