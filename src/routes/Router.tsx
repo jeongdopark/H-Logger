fc085242d2ba8cb5fafd20ec630f9cc0b729ec21
Xeraph <jeongdo.dev@gmail.com>
2023-12-08 02:30:37 +0900

    feat: dynamic import

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,10 +1,15 @@
 import { PATH_NAME } from "../const/path";
 import Layout from "../components/layout/Layout";
 import Landing from "../pages/landing/Landing";
-import Calendar from "../pages/calendar/Calendar";
-import Mission from "../pages/mission/Mission";
-import CalendarDetail from "../pages/detail/Detail";
-import Record from "../pages/record/Record";
+import { Suspense, lazy } from "react";
+import CalendarSkeleton from "../components/skeleton/Calendar";
+import Loading from "../components/Loading";
+import RecordBoxSkeleton from "../components/skeleton/RecordBox";
+
+const Calendar = lazy(() => import("../pages/calendar/Calendar.tsx"));
+const Mission = lazy(() => import("../pages/mission/Mission.tsx"));
+const CalendarDetail = lazy(() => import("../pages/detail/Detail.tsx"));
+const Record = lazy(() => import("../pages/record/Record.tsx"));
 
 export const RouterInfo = [
   {
@@ -18,22 +23,38 @@ export const RouterInfo = [
       },
       {
         path: PATH_NAME.CALENDAR,
-        element: <Calendar />,
+        element: (
+          <Suspense fallback={<CalendarSkeleton />}>
+            <Calendar />
+          </Suspense>
+        ),
         label: "calendar",
       },
       {
         path: PATH_NAME.CALENDAR_DETAIL,
-        element: <CalendarDetail />,
+        element: (
+          <Suspense fallback={<Loading />}>
+            <CalendarDetail />
+          </Suspense>
+        ),
         label: "calendarDetail",
       },
       {
         path: PATH_NAME.MISSION,
-        element: <Mission />,
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Mission />
+          </Suspense>
+        ),
         label: "mission",
       },
       {
         path: PATH_NAME.RECORD,
-        element: <Record />,
+        element: (
+          <Suspense fallback={<RecordBoxSkeleton />}>
+            <Record />
+          </Suspense>
+        ),
         label: "record",
       },
     ],
