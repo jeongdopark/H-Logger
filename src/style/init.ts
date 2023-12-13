a679a9a22911619ab495a14985986ed7aa276bfd
Xeraph <jeongdo.dev@gmail.com>
2023-12-14 02:21:15 +0900

    style: Noto Sans KR 폰트적용

--- src/style/init.ts
+++ src/style/init.ts
@@ -2,9 +2,16 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
 export const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;600;700&display=swap');
+  body{
+    
+    font-family: 'Noto Sans KR', sans-serif !important;
+    
+  }
   *{
     box-sizing: border-box;
   }
+
   li{
     list-style-type: none;
   }
